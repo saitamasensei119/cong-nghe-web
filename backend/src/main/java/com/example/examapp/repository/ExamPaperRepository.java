a387c87170507cd2f0e3e43b20eaea6ee7f0d023
datct <dat8ccxh@gmail.com>
2025-04-20 22:54:41 +0700

    fix

--- backend/src/main/java/com/example/examapp/repository/ExamPaperRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.examapp.repository;
-
-import com.example.examapp.entity.ExamPaper;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExamPaperRepository extends JpaRepository<ExamPaper, Long> {
-}
