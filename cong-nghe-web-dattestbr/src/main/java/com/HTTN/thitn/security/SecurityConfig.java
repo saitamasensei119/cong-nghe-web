7370f048309ee9fa2666f1d4da47534688b005cd
DDat2k4 <tddat19102004@gmail.com>
2025-05-28 23:44:07 +0700

    Add files via upload

--- cong-nghe-web-dattestbr/src/main/java/com/HTTN/thitn/security/SecurityConfig.java
+++ cong-nghe-web-dattestbr/src/main/java/com/HTTN/thitn/security/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/admin/**").hasAuthority("ADMIN")
                         .requestMatchers("/api/teacher/**").hasAuthority("TEACHER")
                         .requestMatchers("/api/student/**").hasAuthority("STUDENT")
+                        .requestMatchers("/api/user/**").authenticated()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
